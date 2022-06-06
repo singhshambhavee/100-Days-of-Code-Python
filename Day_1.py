227839cdbdf3128273fcc0beedaaa0f2da6115c3
Shambhavee Singh <74502776+singhshambhavee@users.noreply.github.com>
2022-06-06 22:46:34 +0530

    Update Day_1.py

--- Day_1.py
+++ Day_1.py
@@ -1,4 +1,5 @@
 # Brand Name Generator
-first_name = input("What's name of the city you grew up in?")
-last_name = input("What's your pet's name?")
-print(first_name + " " + last_name)
+print("Welcome to Band Name generator.")
+first_name = input("What's name of the city you grew up in?\n")
+last_name = input("What's your pet's name?\n")
+print("Your Band Name could be : "first_name + " " + last_name)
